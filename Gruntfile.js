8a35ce3cf1335f83151da80327aa6e40a8c96a45
Santiago <salurralde@ascentio.com.ar>
2017-01-06 14:44:50 -0300

    Added Header

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = function(grunt) {
 
     // Project configuration.
@@ -17,16 +19,59 @@ module.exports = function(grunt) {
             },
             build: {
                 files: {
-                    'app/assets/css/main.css': 'app/modules/**/*.less'
+                    'app/assets/css/main.css': 'app/assets/css/*.less'
                 }
             }
+        },   
+        jshint: {
+            files: ['app/modules/**/*.js', 'Gruntfile.js'],
+            options: {
+                jshintrc: true,
+                force: true
+            }
         },        
+        watch: {
+            script: {
+                files: ['app/modules/**/*.js', 'app/assets/css/*.less'],
+                tasks: ['less', 'jshint'],
+                options: {
+                    spawn: false
+                }
+            },
+            html: {
+                files: ['app/modules/**/*.html'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                script: 'node_modules/http-server/bin/http-server',
+                options: {
+                    nodeArgs: ['--debug'],
+                    ext: 'js,html',
+                }
+            }
+        },
+        concurrent: {
+            default: ['nodemon', 'watch'],
+            debug: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true,
+                limit: 10
+            }
+        }            
     });
 
     // Load the plugins.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'less']);
+    grunt.registerTask('default', ['uglify', 'less', 'concurrent:default']);
 };
